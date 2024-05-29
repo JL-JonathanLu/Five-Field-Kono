8c400e71bbd738c18bbe3cdf68f25b48a0674a44
JL-JonathanLu <702361@pdsb.net>
2024-05-29 11:02:35 -0400

    Add files via upload

---
+++ Five Field Kono Game.py
@@ -0,0 +1,40 @@
+# ===========================================================================
+# PYGAME TEMPLATE
+# Use this as a starting point for your Pygame projects.
+# ===========================================================================
+
+# Imported modules and initialization (add as necessary)
+import pygame as pg
+import board_class
+pg.init()
+
+# Define constants (add as necessary)
+
+# Create a surface for drawing
+surf_width, surf_height = 950, 900
+surface = pg.display.set_mode((surf_width, surf_height))
+pg.display.set_caption("My Pygame Program")
+
+# MAIN GAME LOOP
+running = True
+while running:
+    # Check event queue for actionable items
+    for event in pg.event.get():    
+        # Exit the program
+        if event.type == pg.QUIT:
+            running = False
+#         elif event.type == pg.MOUSEBUTTONDOWN:
+#             if event.button == 1:
+                
+                    
+
+    # Game logic goes here
+    
+    # Drawing commands go here
+    board_class.Board(170).draw(surface)
+    board_class.Game_Piece((50, 140, 25)).draw(surface)
+    # Update display, clock, etc.
+    pg.display.update()
+
+# Shut down Pygame
+pg.quit()
---
+++ board_class.py
@@ -0,0 +1,21 @@
+import pygame as pg
+import random
+
+class Board:
+    def __init__(self, blocksize):
+        self.blocksize = blocksize
+    def draw(self, surface):
+        for x in range(50, 750, self.blocksize):
+            for y in range(25, 750, self.blocksize):
+                rect = pg.Rect(x, y, self.blocksize, self.blocksize)
+                pg.draw.rect(surface, "white", rect, 8)
+
+class Game_Piece:
+    def __init__(self, colour = ()):
+        self.colour = colour
+    def draw(self, surface):
+        pg.draw.circle(surface, self.colour, (55, 705), 5)
+        image = pg.draw.circle(surface, self.colour, (55, 705), 5)
+        image.get_rect()
+
+
