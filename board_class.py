a1dda5fffc29a4de749d31c110708cd75c12bc5d
Dragon34R <793809@pdsb.net>
2024-06-03 14:37:33 -0400

    Update board_class.py

--- board_class.py
+++ board_class.py
@@ -1,24 +1,19 @@
-import pygame as pg
-import random
-
 class Board:
-    def __init__(self, blocksize, surf_width, surf_hight):
+    def __init__(self, blocksize, position = []):
         self.blocksize = blocksize
-        self.surf_width = surf_width
-        self.surf_hight = surf_hight
+        self.position = position
     def draw(self, surface):
-        for x in range(0, surf_width, self.blocksize):
-            for y in range(0, surf_hight, self.blocksize):
+        for x in range(50, 750, self.blocksize):
+            for y in range(25, 750, self.blocksize):
                 rect = pg.Rect(x, y, self.blocksize, self.blocksize)
+                self.position.append((x,y))
                 pg.draw.rect(surface, "white", rect, 8)
+       
 
 class Game_Piece:
     def __init__(self, colour = ()):
         self.colour = colour
     def draw(self, surface):
-        pg.draw.circle(surface, self.colour, (55, 705), 5)
-        image = pg.draw.circle(surface, self.colour, (55, 705), 5)
-        image_x,image_y = image.get_rect()
-        image_x //= 2
+        image = pg.draw.circle(surface, self.colour, (55, 790), 5)
         image_y //= 2
 
