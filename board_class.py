0dad9dfc496282638b7e69a15ed79a4e1ef32a50
JL-JonathanLu <702361@pdsb.net>
2024-06-04 09:44:50 -0400

    Update board_class.py

--- board_class.py
+++ board_class.py
@@ -1,30 +1,38 @@
-class Board:
-    def __init__(self, blocksize, grid = []):
+import pygame as pg
+
+class GamePiece:
+    def draw_piece(self, surface, y_axis, x_axis):
+        print(self.position)
+        if y_axis == 0:
+            pg.draw.circle(surface, "red", (self.position[x_axis][0] + 85, self.position[x_axis][1]+80), 60)
+        elif y_axis == 1:
+            if x_axis == 0 or x_axis == 4:
+                pg.draw.circle(surface, "red", (self.position[x_axis][0]+85 , self.position[x_axis][1]+80), 60)
+        elif y_axis == 3:
+            if x_axis == 0 or x_axis == 4:
+                pg.draw.circle(surface, "blue", (self.position[x_axis][0]+85 , self.position[x_axis][1]+80), 60)
+        elif y_axis == 4:
+            pg.draw.circle(surface, "blue", (self.position[x_axis][0] + 85, self.position[x_axis][1]+80), 60)
+        
+class Board(GamePiece):
+    def __init__(self, blocksize = 170 , grid = [], position = []):
         self.blocksize = blocksize
         self.grid = grid
+        self.position = position
     def draw_grid(self, surface):
-        x_axi = 0
-        for x in range(50, 750, self.blocksize):
-            position = []
-            y_axi = 0
-            for y in range(25, 750, self.blocksize):
+        y_axis = 0
+        for y in range(25, 750, self.blocksize):
+            self.position = []
+            x_axis = 0
+            for x in range(50, 750, self.blocksize):
                 rect = pg.Rect(x, y, self.blocksize, self.blocksize)
                 pg.draw.rect(surface, "white", rect, 8)
-                position.append((x,y))
-                y_axi += 1
-            self.grid.append(position)
-            x_axi += 1
+                self.position.append((x,y))
+                GamePiece.draw_piece(self, surface, y_axis, x_axis)
+                x_axis += 1
+            self.grid.append(self.position)
+            y_axis += 1
 
-        # check if the self.grid
-        for i  in range(0,len(self.grid)):
-            print(self.grid[i])
-       
-
-class GamePiece(Board):
-    def __init__(self, colour = ()):
-        super().__init__(self, blocksize, grid = [])
-        self.colour = colour
-    def draw_piece(self, surface):
-        image = pg.draw.circle(surface, self.colour, (55, 790), 5)
-        image_y //= 2
-
+#         # check if the self.grid
+#         for i  in range(0,len(self.grid)):
+#             print(self.grid[i])
