a8d2cc21ba2d64000ddb887975952df22bbeb535
Dragon34R <793809@pdsb.net>
2024-06-05 18:07:54 -0400

    Update Five Field Kono Game.py

--- Five Field Kono Game.py
+++ Five Field Kono Game.py
@@ -10,11 +10,19 @@ pg.init()
 
 # Define constants (add as necessary)
 surf_width, surf_height = 950, 900
+
 # Create a surface for drawing
-p1 = board_class.GamePiece()
-p2 = board_class.GamePiece()
 surface = pg.display.set_mode((surf_width, surf_height))
 pg.display.set_caption("My Pygame Program")
+
+# Creating player's pieces 
+side1 = pg.image.load("images/player_1.png")
+side1_rect = side1.get_rect()
+side1 = pg.transform.scale(side1, (side1_rect.width * 0.025, side1_rect.height * 0.025))
+side2 = pg.image.load("images/player_2.png")
+side2_rect = side2.get_rect()
+side2 = pg.transform.scale(side2, (side2_rect.width * 0.025, side2_rect.height * 0.025))
+
 
 # MAIN GAME LOOP
 running = True
@@ -25,15 +33,16 @@ while running:
         if event.type == pg.QUIT:
             running = False
         elif event.type == pg.MOUSEBUTTONDOWN:
-            if p1.rect.
-                if event.button == 1:
-                
-                
-                    
+                if p1.rect.collidepoint(event.pos):
+                    if event.button == 1:
+                        print("click")
+               
+                   
 
     # Game logic goes here
-    
+   
     # Drawing commands go here
+    p1 = board_class.GamePiece()
     board_class.Board().draw_grid(surface)
     # Update display, clock, etc.
     pg.display.update()
