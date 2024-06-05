4b70899d246b2257524b850c1a89412e778fa8f4
Dragon34R <793809@pdsb.net>
2024-06-05 18:01:56 -0400

    Update board_class.py

--- board_class.py
+++ board_class.py
@@ -1,24 +1,28 @@
 import pygame as pg
 
 class GamePiece:
-    def draw_piece(self, surface, y_axis, x_axis):
-        print(self.position)
+    def __init__(self, player1 = [], player2 = []):
+        self.player1 = player1
+        self.player2 = player2
+    def draw_piece(self, surface, y_axis, x_axis):        
         if y_axis == 0:
-            pg.draw.circle(surface, "red", (self.position[x_axis][0] + 85, self.position[x_axis][1]+80), 60)
+            surface.blit(side1, (self.position[x_axis][0], self.position[x_axis][1]))
         elif y_axis == 1:
             if x_axis == 0 or x_axis == 4:
-                pg.draw.circle(surface, "red", (self.position[x_axis][0]+85 , self.position[x_axis][1]+80), 60)
+                surface.blit(side1, (self.position[x_axis][0], self.position[x_axis][1]))
         elif y_axis == 3:
             if x_axis == 0 or x_axis == 4:
-                pg.draw.circle(surface, "blue", (self.position[x_axis][0]+85 , self.position[x_axis][1]+80), 60)
+                surface.blit(side2, (self.position[x_axis][0], self.position[x_axis][1]))
         elif y_axis == 4:
-            pg.draw.circle(surface, "blue", (self.position[x_axis][0] + 85, self.position[x_axis][1]+80), 60)
-        
+            surface.blit(side2, (self.position[x_axis][0], self.position[x_axis][1]))
+       
 class Board(GamePiece):
-    def __init__(self, blocksize = 170 , grid = [], position = []):
+    def __init__(self, blocksize = 170 , grid = [], position = [], side1, side2):
         self.blocksize = blocksize
         self.grid = grid
         self.position = position
+        self.player1 = player1
+        self.player2 = player2
     def draw_grid(self, surface):
         y_axis = 0
         for y in range(25, 750, self.blocksize):
@@ -32,10 +36,10 @@ class Board(GamePiece):
                 x_axis += 1
             self.grid.append(self.position)
             y_axis += 1
-
-#         # check if the self.grid
-#         for i  in range(0,len(self.grid)):
-#             print(self.grid[i])
+        return self.player1, self.player2
+    def turns(self):
+        turn = 0
+        move_x, move_y = 0, 0
 
 
 
