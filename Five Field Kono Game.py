b074720a478f54ed3305eea3f541fce2f493ff08
JL-JonathanLu <702361@pdsb.net>
2024-06-06 09:48:31 -0400

    Update Five Field Kono Game.py

--- Five Field Kono Game.py
+++ Five Field Kono Game.py
@@ -10,6 +10,9 @@ pg.init()
 
 # Define constants (add as necessary)
 surf_width, surf_height = 950, 900
+grid = []
+position = []
+blocksize = 170
 
 # Create a surface for drawing
 surface = pg.display.set_mode((surf_width, surf_height))
@@ -18,11 +21,14 @@ pg.display.set_caption("My Pygame Program")
 # Creating player's pieces 
 side1 = pg.image.load("images/player_1.png")
 side1_rect = side1.get_rect()
-side1 = pg.transform.scale(side1, (side1_rect.width * 0.025, side1_rect.height * 0.025))
+side1 = pg.transform.scale(side1, (side1_rect.width * 0.31, side1_rect.height * 0.31))
+side1_rect.size = side1.get_size()
 side2 = pg.image.load("images/player_2.png")
 side2_rect = side2.get_rect()
-side2 = pg.transform.scale(side2, (side2_rect.width * 0.025, side2_rect.height * 0.025))
-
+side2 = pg.transform.scale(side2, (side2_rect.width * 0.31, side2_rect.height * 0.31))
+side2_rect.size = side2.get_size()
+sprite_group1 = pg.sprite.Group
+sprite_group2 = pg.sprite.Group
 
 # MAIN GAME LOOP
 running = True
@@ -33,8 +39,10 @@ while running:
         if event.type == pg.QUIT:
             running = False
         elif event.type == pg.MOUSEBUTTONDOWN:
-                if p1.rect.collidepoint(event.pos):
-                    if event.button == 1:
+            if event.button == 1:
+                mouse = pg.mouse.get_pos()
+                for i in sprite_group1:
+                    if side1_rect.collidepoint(mouse):
                         print("click")
                
                    
@@ -43,7 +51,7 @@ while running:
    
     # Drawing commands go here
     p1 = board_class.GamePiece()
-    board_class.Board().draw_grid(surface)
+    board_class.Board(blocksize, grid, position, side1, side2, sprite_group1, sprite_group2).draw_grid(surface)
     # Update display, clock, etc.
     pg.display.update()
 
