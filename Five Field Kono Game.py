50c8be980d6ee7d2e2b78c80afc84a467d3e9ba8
Dragon34R <793809@pdsb.net>
2024-06-09 11:21:22 -0400

    Update Five Field Kono Game.py

--- Five Field Kono Game.py
+++ Five Field Kono Game.py
@@ -10,25 +10,34 @@ pg.init()
 
 # Define constants (add as necessary)
 surf_width, surf_height = 950, 900
-grid = []
-position = []
+position = [0,0]
+grid =  [[[2,position],[2,position],[2,position],[2,position],[2,position]],
+            [[2,position],[0,position],[0,position],[0,position],[2,position]],
+            [[0,position],[0,position],[0,position],[0,position],[0,position]],
+            [[1,position],[0,position],[0,position],[0,position],[1,position]],
+            [[1,position],[1,position],[1,position],[1,position],[1,position]]]
+
+win_condition_1 = [[1,position],[1,position],[1,position],[1,position],[1,position],
+                               [1,position],[0,position],[0,position],[0,position],[1,position]]
+
+win_condition_2 = [[2,position],[0,position],[0,position],[0,position],[2,position],
+                            [2,position],[2,position],[2,position],[2,position],[2,position]]
+
 blocksize = 170
 
 # Create a surface for drawing
 surface = pg.display.set_mode((surf_width, surf_height))
 pg.display.set_caption("My Pygame Program")
 
-# Creating player's pieces 
+# Creating player's pieces
 side1 = pg.image.load("images/player_1.png")
 side1_rect = side1.get_rect()
 side1 = pg.transform.scale(side1, (side1_rect.width * 0.31, side1_rect.height * 0.31))
-side1_rect.size = side1.get_size()
+side1_rect_size = side1.get_size()
 side2 = pg.image.load("images/player_2.png")
 side2_rect = side2.get_rect()
 side2 = pg.transform.scale(side2, (side2_rect.width * 0.31, side2_rect.height * 0.31))
-side2_rect.size = side2.get_size()
-sprite_group1 = pg.sprite.Group
-sprite_group2 = pg.sprite.Group
+side2_rect_size = side2.get_size()
 
 # MAIN GAME LOOP
 running = True
@@ -44,14 +53,12 @@ while running:
                 for i in sprite_group1:
                     if side1_rect.collidepoint(mouse):
                         print("click")
-               
                    
 
     # Game logic goes here
    
     # Drawing commands go here
-    p1 = board_class.GamePiece()
-    board_class.Board(blocksize, grid, position, side1, side2, sprite_group1, sprite_group2).draw_grid(surface)
+    board_class.Board(blocksize, grid, position, side1, side2).draw_grid(surface)
     # Update display, clock, etc.
     pg.display.update()
 
